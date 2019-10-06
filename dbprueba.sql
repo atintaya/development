b1125c0ca1c541adca0700a4e66fec90b4e8260f
antonia tintaya <antonia.tintaya@gmail.com>
2019-10-06 09:37:10 -0500

    versión 3

---
+++ dbprueba.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Ultimate v10.42 
+MySQL - 5.7.24 : Database - dbprueba
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dbprueba` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
+
+USE `dbprueba`;
+
+/*Table structure for table `suma` */
+
+DROP TABLE IF EXISTS `suma`;
+
+CREATE TABLE `suma` (
+  `sumando01` int(11) DEFAULT NULL,
+  `sumando02` int(11) DEFAULT NULL,
+  `resultado` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+/*Data for the table `suma` */
+
+LOCK TABLES `suma` WRITE;
+
+insert  into `suma`(`sumando01`,`sumando02`,`resultado`) values (1,3,4),(45,12,57),(7,6,13),(7,6,13);
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
